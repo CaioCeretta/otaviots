a568e99c6b99af9ab49821a1a47711f1b4b95b76
Caio Ceretta <caioceretta@gmail.com>
2022-10-12 14:47:59 -0300

    Added the theory of the Abstract Factory method

---
+++ src/Design Patterns - GoF/creational/abstract-factory/index.ts
@@ -0,0 +1,17 @@
+/* Ele é bem similar ao factory, a intenção dele é fornecer uma interface para a criação de famílias de objetos
+  relacionados ou dependentes sem especificar as classes concretas
+  Tem uma camada a mais de abstração e cria factories específicas para famílias de objetos
+  É composto por vários factory methods
+  Visa agrupar famílias de objetos compatíveis e cria uma fábrica concreta por grupos de objetos compatíveis
+
+  Aplicabilidade:
+
+  Devemos usar o abstract factory quando:
+
+  . Um sistema deve ser independente de como seus produtos são criados, compostos ou representados
+  . Um sistema deve ser configurado com uma família de produtos que podem(ou não) trabalhar juntos
+  . Uma família de objetos for projetada para ser usada em conjunto e você necessita garantir essa restrição
+  . Você quer fornecer uma biblioteca de classes de produtos e quer revelar somente suas interfaces
+    Não suas implementações
+
+  */
