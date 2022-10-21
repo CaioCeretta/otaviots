202acf04bb5242836631c3860c18e19cfa6518f8
Caio Ceretta <caioceretta@gmail.com>
2022-10-21 00:01:02 -0300

    Added the theory part of the Mediator pattern

---
+++ src/Design Patterns - GoF/behavioral/mediator/index.ts
@@ -0,0 +1,30 @@
+/* Intenção Oficial
+
+Define um objeto que encapsula como um conjunto de objetos interage. O mediator promove o acoplamento fraco ao evitar
+que os objetos se refiram explicitamente uns aos outros, permitindo que você varie suas interações
+
+Sobre o Mediadator
+
+. Visa encapsular a comunicação direta entre objetos
+. Desacopla objetos que estariam intimamente ligados
+. Centraliza toda comunicação em apenas um objeto
+
+Aplicabilidade
+
+Use o Mediator quando:
+
+. Você quer diminuir ou extinguir o acoplamento direto entre as classes que poderiam estar diretamente acopladas
+. Você quer simplificar comunicações de muitos-para-muitos para comunicações um-para-muitos
+
+Lado bom
+
+. Desacopla objetos que poderiam estar firmemente acoplados
+. Facilita a reutilização de objetos
+. Facilita a adição de novos mediadores e classes participantes na comunicação
+. Encapsula a comunicação entre objetos
+
+Lado ruim
+
+. É facil criar um mediator "faz tudo" (god class)
+
+*/
